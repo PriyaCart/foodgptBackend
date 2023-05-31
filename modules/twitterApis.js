5de47c085c9f9bf4f80781b30a8c2e097df6dc27
PriyaCart <priya@fidisys.com>
2023-05-31 15:06:50 +0530

    token issue solved

--- modules/twitterApis.js
+++ modules/twitterApis.js
@@ -5,14 +5,14 @@ const { Configuration, OpenAIApi } = require("openai");
 module.exports = {
   async breakfastList (req,res){
     try{
-     // console.log("be entered")
+    //  console.log("be entered")
       const place = req.query.place;
      // console.log(place, "place")
-      let random = Math.floor(Math.random() * 6) + 8;
+      let random = Math.floor(Math.random() * 5) + 8;
       let cityName;
       let cityResponse;
       const configuration = new Configuration({
-        apiKey: "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"
+        apiKey: "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"
       });
     
       const openai = new OpenAIApi(configuration);
@@ -51,7 +51,7 @@ module.exports = {
   }
     }
     catch(err){
-     // console.log(err.message, "err")
+    //  console.log(err.message, "err")
       return res.send({error:err.message})
  }
   },
@@ -63,37 +63,44 @@ module.exports = {
       let dishArray = [];
       dish = req.query.dish;
       dishArray = dish.split(',');
+     // console.log(dishArray)
       let bfArray = [];
       const configuration = new Configuration({
-        apiKey: "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"
+        apiKey: "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"
       });
       const openai = new OpenAIApi(configuration);
       for(let i=0; i<dishArray.length; i++){
-      const dishContent = await openai.createCompletion({
-          model: "text-davinci-003",
-          prompt:  `Describe about ${dishArray[i]} dish in 30 words. For example, Ghari is a sweet dish from Surat, India. It is round in shape, white in color, and has a soft and chewy texture. It is made with sweetened mawa (milk solids), ghee, sugar, and flavored with cardamom. context: Please explain how it looks like for example color, shape and texture. explain the recipe.`,
-          prompt:  `Can you explain the appearance of ${dishArray[i]} dish in 40 words? context: It should also include color, shape, size and lookwise`,
-          temperature: 0,
-          max_tokens: 500,
-          top_p: 1.0,
-          frequency_penalty: 0.5,
-          presence_penalty: 0.0,
-        });
-      console.log(dishContent.data.choices[0].text, "dish message");
-      const dishName = dishContent.data.choices[0].text;
-      const response = await openai.createImage({
-        prompt: dishName,
+       // console.log(dishArray[i], '123')
+        // const dishContent = await openai.createChatCompletion({
+        //   model: "gpt-3.5-turbo",
+        //   temperature: 0,
+        //   messages: [{role: "user", content: `Can you explain the appearance of ${dishArray[i]} dish in 40 words? context: It should also include color, shape, size and lookwise`}],
+        // });
+      // const dishContent = await openai.createCompletion({z
+      //     model: "text-davinci-003",
+      //     prompt:  `Can you explain the appearance of ${dishArray[i]} dish in 40 words? context: It should also include color, shape, size and lookwise`,
+      //     temperature: 0,
+      //     max_tokens: 500,
+      //     top_p: 1.0,
+      //     frequency_penalty: 0.5,
+      //     presence_penalty: 0.0,
+      //   });
+     // console.log(dishContent.data.choices[0].text, "dish message");
+      //  const dishName = dishContent.data.choices[0].text;
+      //  const dishName =   dishContent.data.choices[0].message.content;
+        const response = await openai.createImage({
+        prompt: `${dishArray[i]} dish`,
         n: 1,
         size: "256x256",
       });
       image_url = response.data.data[0].url;
       bfArray.push(image_url)
     }
-     // console.log(bfArray.length, "imgurl1")
+      //console.log(bfArray, "imgurl1")
       return res.send({dish:dishArray, img:bfArray})
     }
     catch(err){
-    //  console.log(err, "ERROR")
+        // console.log(err, "ERROR")
          return res.send(err)
     }
   },
@@ -104,34 +111,34 @@ module.exports = {
       dishArray = dish.split(',');
       let bfArray = [];
       const configuration = new Configuration({
-        apiKey: "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"
+        apiKey: "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"
       });
       const openai = new OpenAIApi(configuration);
       for(let i=0; i<dishArray.length; i++){
-      const dishContent = await openai.createCompletion({
-          model: "text-davinci-003",
-          prompt:  `Describe about ${dishArray[i]} dish in 30 words. For example, Ghari is a sweet dish from Surat, India. It is round in shape, white in color, and has a soft and chewy texture. It is made with sweetened mawa (milk solids), ghee, sugar, and flavored with cardamom. context: Please explain how it looks like for example color, shape and texture. explain the recipe.`,
-          temperature: 0,
-          max_tokens: 500,
-          top_p: 1.0,
-          frequency_penalty: 0.5,
-          presence_penalty: 0.0,
-        });
-      console.log(dishContent.data.choices[0].text, "dish message");
-      const dishName = dishContent.data.choices[0].text;
+      // const dishContent = await openai.createCompletion({
+      //     model: "text-davinci-003",
+      //     prompt:  `Describe about ${dishArray[i]} dish in 30 words. For example, Ghari is a sweet dish from Surat, India. It is round in shape, white in color, and has a soft and chewy texture. It is made with sweetened mawa (milk solids), ghee, sugar, and flavored with cardamom. context: Please explain how it looks like for example color, shape and texture. explain the recipe.`,
+      //     temperature: 0,
+      //     max_tokens: 500,
+      //     top_p: 1.0,
+      //     frequency_penalty: 0.5,
+      //     presence_penalty: 0.0,
+      //   });
+      //console.log(dishContent.data.choices[0].text, "dish message");
+    //  const dishName = dishContent.data.choices[0].text;
       const response = await openai.createImage({
-        prompt: dishName,
+        prompt: `${dishArray[i]} dish`,
         n: 1,
         size: "256x256",
       });
       image_url = response.data.data[0].url;
       bfArray.push(image_url)
     }
-    // console.log(bfArray, "imgurl")
+      ///console.log(bfArray, "imgurl")
       return res.send({dish:dishArray, img:bfArray})
     }
     catch(err){
-    //  console.log(err, "ERROR")
+       //  console.log(err, "ERROR")
          return res.send(err)
     }
   },
@@ -142,33 +149,34 @@ module.exports = {
       dishArray = dish.split(',');
       let bfArray = [];
       const configuration = new Configuration({
-        apiKey: "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"
+        apiKey: "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"
       });
       const openai = new OpenAIApi(configuration);
       for(let i=0; i<dishArray.length; i++){
-      const dishContent = await openai.createCompletion({
-          model: "text-davinci-003",
-          prompt:  `Describe about ${dishArray[i]} dish in 30 words. For example, Ghari is a sweet dish from Surat, India. It is round in shape, white in color, and has a soft and chewy texture. It is made with sweetened mawa (milk solids), ghee, sugar, and flavored with cardamom. context: Please explain how it looks like for example color, shape and texture. explain the recipe.`,
-          temperature: 0,
-          max_tokens: 500,
-          top_p: 1.0,
-          frequency_penalty: 0.5,
-          presence_penalty: 0.0,
-        });
-      console.log(dishContent.data.choices[0].text, "dish message");
-      const dishName = dishContent.data.choices[0].text;
+      // const dishContent = await openai.createCompletion({
+      //     model: "text-davinci-003",
+      //     prompt:  `Describe about ${dishArray[i]} dish in 30 words. For example, Ghari is a sweet dish from Surat, India. It is round in shape, white in color, and has a soft and chewy texture. It is made with sweetened mawa (milk solids), ghee, sugar, and flavored with cardamom. context: Please explain how it looks like for example color, shape and texture. explain the recipe.`,
+      //     temperature: 0,
+      //     max_tokens: 500,
+      //     top_p: 1.0,
+      //     frequency_penalty: 0.5,
+      //     presence_penalty: 0.0,
+      //   });
+      //console.log(dishContent.data.choices[0].text, "dish message");
+    //  const dishName = dishContent.data.choices[0].text;
       const response = await openai.createImage({
-        prompt: dishName,
+        prompt: `${dishArray[i]} dish`,
         n: 1,
         size: "256x256",
       });
       image_url = response.data.data[0].url;
       bfArray.push(image_url)
     }
+    console.log(bfArray, "imgurl3")
     return res.send({dish:dishArray, img:bfArray})
     }
     catch(err){
-    //  console.log(err, "ERROR")
+      //   console.log(err, "ERROR")
          return res.send(err)
     }
   },
